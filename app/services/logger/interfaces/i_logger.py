cdbc61dd8d871f06f895da551a6f532043f0f806
cedrata <81557163+cedrata@users.noreply.github.com>
2022-05-29 20:14:35 +0200

    [UPDATE] code cleaning, simpler interface.

--- app/services/logger/interfaces/i_logger.py
+++ app/services/logger/interfaces/i_logger.py
@@ -13,48 +13,28 @@ class ILogger(ZInterface):
         ZInterface (zope.interface.Interface): Interface class from zope.interface.
     """
 
-
-
-    def add_logger(logger_name: str, configuration: Optional[str] = None) -> None:
+    def add_logger(logger_name: str) -> None:
         """
-        Create a new logger with the given name, and configured if configuration
-        is assigned.
+        Create a new logger with the given name.
 
         Args:
-            logger_name (str): _description_
-            configuration (Optional[str]): path of the configuration file. Default to None.
+            logger_name (str): logger name.
         """
 
-    def file_config(logger_name: str, configuration: str) -> None:
+    def file_config(config_file_path: str) -> None:
         """
         Configure the given logger with a valid configuration file.
 
         Args:
-            logger_name (str): name of the logger to configure.
-            configuration (str): path of the configuration file.
+            config_file_path (str): absolute path of the configuration file.
         """
 
-    def set_logger_level(logger_name: str, level: LogLevel) -> None:
+    def print_log(logger_name: str, logger_level: LogLevel, message: str) -> None:
         """
-        Set the given name logger log level with the given one.
-
-        Args:
-            logger_name (str): name of the logger to change level.
-            level (LogLevel): the new log level.
-        """
-
-    def set_format(format: str) -> None:
-        """
-        Set a new format for all the loggers.
+        Print a log statement with the specified logger with the given level.
 
         Args:
-            logger_name (str): the new log format.
-        """
-
-    def exists(logger_name: str) -> bool:
-        """
-        Check if any logger matching the given name exists.
-
-        Args:
-            logger_name (str): name of the logger to search
+            logger_name (str): the logger name to use.
+            logger_level (str): the log level to print.
+            message (str): the message to log.
         """
