40238db75c02fa13281bb641fc11b6c5dcf3e3f6
cedrata <81557163+cedrata@users.noreply.github.com>
2022-07-16 21:36:00 +0200

    [UPDATE] doc improvements.

--- src/routes/auth.py
+++ src/routes/auth.py
@@ -5,21 +5,19 @@ from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 
 # from src.helpers.container import CONTAINER
-# from typing import Any
 from fastapi.security import OAuth2PasswordRequestForm
 from src.mock.fake_db import DB_USERS
 from src.models.auth import AuthMessage
+from src.models.commons import HttpExceptionMessage
 
-# from src.mock.fake_db import DB_USERS
 from src.routes.enums.commons import Endpoint
-from src.services.auth import OAUTH2_SCHEME
 
 # Constant initialization.
 _LOGIN_POST_PARAMS: Final[Dict[Endpoint, Any]] = {
     Endpoint.RESPONSE_MODEL: AuthMessage,
     Endpoint.RESPONSES: {
         status.HTTP_401_UNAUTHORIZED: {
-            # "model": Any if required (not valid to isert http exception here)
+            "model": HttpExceptionMessage,
             "description": "Unsuccesful login, wrong email or password",
         }
     },
@@ -51,7 +49,9 @@ async def login(request_form: OAuth2PasswordRequestForm = Depends()):
         raise HTTPException(status.HTTP_401_UNAUTHORIZED, detail=error_message)
 
     # The user exists.
-    response = AuthMessage(access_token="success", refresh_token="success", token_type="bearer")
+    response = AuthMessage(
+        access_token="success", refresh_token="success", token_type="bearer"
+    )
     status_code = status.HTTP_200_OK
 
     return JSONResponse(status_code=status_code, content=jsonable_encoder(response))
