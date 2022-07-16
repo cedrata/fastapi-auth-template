71f93901acae067a64cf19f178047e8643781484
cedrata <81557163+cedrata@users.noreply.github.com>
2022-07-16 17:48:51 +0200

    [ADD] oauth2 scheme to rquire login where needed.

---
+++ src/services/auth/__init__.py
@@ -0,0 +1,5 @@
+from fastapi.security import OAuth2PasswordBearer
+from typing import Final
+
+# TODO: move token url to configs.
+OAUTH2_SCHEME: Final[OAuth2PasswordBearer] = OAuth2PasswordBearer(tokenUrl="/login")
