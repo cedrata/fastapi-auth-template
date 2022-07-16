67726fc8b7b63090a7d7b17994c8755aec0b7a02
cedrata <81557163+cedrata@users.noreply.github.com>
2022-07-16 17:51:24 +0200

    [ADD] oauth2 scheme to add auth DI where needed.

--- src/services/auth/__init__.py
+++ src/services/auth/__init__.py
@@ -2,4 +2,4 @@ from fastapi.security import OAuth2PasswordBearer
 from typing import Final
 
 # TODO: move token url to configs.
-OAUTH2_SCHEME: Final[OAuth2PasswordBearer] = OAuth2PasswordBearer(tokenUrl="/login")
+OAUTH2_SCHEME: Final[OAuth2PasswordBearer] = OAuth2PasswordBearer(tokenUrl="/auth/login")
