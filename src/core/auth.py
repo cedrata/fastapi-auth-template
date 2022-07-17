f3a3db73f637b7924a0e6a769944903e175ac5fc
cedrata <81557163+cedrata@users.noreply.github.com>
2022-07-17 23:45:25 +0200

    [ADD] create_token function. Generate tokens w/ secret for access and refresh.

--- src/core/auth.py
+++ src/core/auth.py
@@ -1,5 +1,8 @@
-from fastapi.security import OAuth2PasswordBearer
+from datetime import datetime, timedelta
 from typing import Final
+
+from fastapi.security import OAuth2PasswordBearer
+from jose import jwt
 
 # This is instance will be injected in routes when those have to be secured.
 # This is just checcking the user exists and the inserted credentials are corect, role is not
@@ -8,7 +11,7 @@ from typing import Final
 # The tokenUrl is not inserted in any configuration file because it should not change in time,
 # by doing so it encourages keep the same endpoint as the login one.
 OAUTH2_SCHEME: Final[OAuth2PasswordBearer] = OAuth2PasswordBearer(
-    tokenUrl="/auth/login"
+    tokenUrl="/auth/login",
 )
 
 
@@ -16,3 +19,24 @@ def hash_password(password: str) -> str:
     """Returning the given password with hash."""
     # TODO: Implement password hashing.
     return password
+
+
+def create_token(
+    data: dict, expires_delta: timedelta, secret_key: str, algorithm: str
+) -> str:
+    """Return a token for the given data, this function can be used to return both access and refresh tokens.
+
+    Args:
+        data (dict): data to encode in jwt.
+        expires_delta (timedelta): expiration time expressed in timedelta.
+        secret_key (str): secret key to apply signature to jwt.
+        algorithm (str): desired encription algoritm.
+
+    Returns:
+        str: _description_
+    """
+    to_encode = data.copy()
+    expire = datetime.utcnow() + expires_delta
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, secret_key, algorithm=algorithm)
+    return encoded_jwt
--- src/routes/auth.py
+++ src/routes/auth.py
@@ -1,4 +1,5 @@
 import sys
+from datetime import timedelta
 from os import environ
 from os.path import join
 from typing import Any, Dict, Final
@@ -7,6 +8,7 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 from fastapi.security import OAuth2PasswordRequestForm
+from jose import JWTError
 from src.core import auth
 from src.helpers.container import CONTAINER
 from src.mock.fake_db import DB_USERS
@@ -37,7 +39,11 @@ _LOGIN_POST_PARAMS: Final[Dict[Endpoint, Any]] = {
         status.HTTP_401_UNAUTHORIZED: {
             "model": HttpExceptionMessage,
             "description": "Unsuccesful login, wrong email or password",
-        }
+        },
+        status.HTTP_500_INTERNAL_SERVER_ERROR: {
+            "model": HttpExceptionMessage,
+            "description": "Probably an errorr occured during the token creation",
+        },
     },
     Endpoint.DESCRIPTION: "Authenticate an user given username and password to return a set of access and refresh tokens after a succesful validation.",
 }
@@ -53,6 +59,7 @@ router = APIRouter()
     description=_LOGIN_POST_PARAMS[Endpoint.DESCRIPTION],
 )
 async def login(request_form: OAuth2PasswordRequestForm = Depends()):
+    logger = CONTAINER.get(ILogger)
     response: Any
     status_code: int
 
@@ -70,8 +77,40 @@ async def login(request_form: OAuth2PasswordRequestForm = Depends()):
         raise HTTPException(status.HTTP_401_UNAUTHORIZED, detail=error_message)
 
     # The user exists.
+    # Converting expriation times to timedelta.
+    try:
+        access_timedelta = timedelta(minutes=_JWT_CONFIG["access_expiration"])
+        refresh_timedelta = timedelta(minutes=_JWT_CONFIG["refresh_expiration"])
+    except KeyError as e:
+        msg = f"An error occured while retriving the tokens expiration times"
+        logger.error("auth", f"{msg}: {e}")
+        raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR, detail=msg)
+
+    # Generating access and refresh tokens.
+    try:
+        access_token = auth.create_token(
+            DB_USERS[request_form.username],
+            access_timedelta,
+            environ["SECRET_KEY"],
+            _JWT_CONFIG["algorithm"],
+        )
+        refresh_token = auth.create_token(
+            DB_USERS[request_form.username],
+            refresh_timedelta,
+            environ["SECRET_KEY"],
+            _JWT_CONFIG["algorithm"],
+        )
+    except KeyError as e:
+        msg = f"An error occured while retriving the secret or the algorithm to encode the tokens"
+        logger.error("auth", f"{msg}: {e}")
+        raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR, detail=msg)
+    except JWTError as e:
+        msg = f"An error occured while encoding the tokens"
+        logger.error("auth", f"{msg}: {e}")
+        raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR, detail=msg)
+
     response = AuthMessage(
-        access_token="success", refresh_token="success", token_type="bearer"
+        access_token=access_token, refresh_token=refresh_token, token_type="bearer"
     )
     status_code = status.HTTP_200_OK
 
