5ca532c78a863240035cc175c18f11a7bbfe69de
cedrata <81557163+cedrata@users.noreply.github.com>
2022-07-27 01:07:06 +0200

    [UPDATE] project directory for cleaner testing.

--- src/models/auth.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-
-class AuthMessage(BaseModel):
-    """Class that extend the StatusMessage to access and refresh token."""
-
-    access_token: str
-    refresh_token: str
-    token_type: str
---
+++ tests/routes/test_auth_routes.py
@@ -0,0 +1,6 @@
+# from tests import fastapi_client
+
+
+def test_login():
+    # response = fastapi_client.post("/auth/login", json={"username": "admin", "password": "admin"})
+    assert False
