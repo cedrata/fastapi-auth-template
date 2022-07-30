23b84a4a9bb688d5653c861359a3caeabb7a175f
cedrata <81557163+cedrata@users.noreply.github.com>
2022-07-30 18:09:13 +0200

    [UPDATE] app modulte to suit test.

--- api/src/app.py
+++ api/src/app.py
@@ -10,14 +10,14 @@ reoutes_prefix: Final[str] = "/cdrt"
 
 fastapi_app = FastAPI()
 
+# Injecting routers into app.
+fastapi_app.include_router(
+    hello_world_router, prefix=reoutes_prefix, tags=["Hello, world!"]
+)
+fastapi_app.include_router(auth_router, prefix="/auth", tags=["Auth"])
+
 
 @fastapi_app.on_event("startup")
 async def app_init():
     # Execute db connection.
     await build_client()
-
-    # Injecting routers into app.
-    fastapi_app.include_router(
-        hello_world_router, prefix=reoutes_prefix, tags=["Hello, world!"]
-    )
-    fastapi_app.include_router(auth_router, prefix="/auth", tags=["Auth"])
