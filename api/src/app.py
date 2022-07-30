5090938b2bfa8c0a73e194f867083942d456a93d
cedrata <81557163+cedrata@users.noreply.github.com>
2022-07-30 18:16:40 +0200

    [UPDATE] remove unneded boilerplate code.

--- api/src/app.py
+++ api/src/app.py
@@ -6,13 +6,11 @@ from src.db.connection import build_client
 from src.routes.auth import router as auth_router
 from src.routes.hello_world import router as hello_world_router
 
-reoutes_prefix: Final[str] = "/cdrt"
-
 fastapi_app = FastAPI()
 
 # Injecting routers into app.
 fastapi_app.include_router(
-    hello_world_router, prefix=reoutes_prefix, tags=["Hello, world!"]
+    hello_world_router, prefix="/cdrt", tags=["Hello, world!"]
 )
 fastapi_app.include_router(auth_router, prefix="/auth", tags=["Auth"])
 
