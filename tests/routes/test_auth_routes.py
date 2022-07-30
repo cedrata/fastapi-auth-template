621572a84a0367be2a6a1bfb7cbb5492283d8d4c
cedrata <81557163+cedrata@users.noreply.github.com>
2022-07-30 23:51:56 +0200

    [ADD] minimum test for refresh.

--- tests/routes/test_auth_routes.py
+++ tests/routes/test_auth_routes.py
@@ -2,13 +2,14 @@ import json
 
 import pytest
 from httpx import AsyncClient
-from tests import build_db_client, fastapi_app
+from src.routes.auth import login
+from tests import build_db_client, fastapi_app, BASE_URL
 
 
 @pytest.mark.asyncio
 async def test_login():
     await build_db_client()
-    async with AsyncClient(app=fastapi_app, base_url="http://test") as ac:
+    async with AsyncClient(app=fastapi_app, base_url=BASE_URL) as ac:
         response = await ac.post(
             "/auth/login",
             data={
@@ -30,7 +31,7 @@ async def test_login():
 @pytest.mark.asyncio
 async def test_bad_username_login():
     await build_db_client()
-    async with AsyncClient(app=fastapi_app, base_url="http://test") as ac:
+    async with AsyncClient(app=fastapi_app, base_url=BASE_URL) as ac:
         response = await ac.post(
             "/auth/login",
             data={
@@ -48,7 +49,7 @@ async def test_bad_username_login():
 @pytest.mark.asyncio
 async def test_bad_password_login():
     await build_db_client()
-    async with AsyncClient(app=fastapi_app, base_url="http://test") as ac:
+    async with AsyncClient(app=fastapi_app, base_url=BASE_URL) as ac:
         response = await ac.post(
             "/auth/login",
             data={
@@ -61,3 +62,64 @@ async def test_bad_password_login():
             },
         )
     assert response.status_code == 401
+
+
+@pytest.mark.asyncio
+async def test_refresh():
+    await build_db_client()
+
+    # Generate a valid token.
+    async with AsyncClient(app=fastapi_app, base_url=BASE_URL) as ac:
+        login_response = await ac.post(
+            "/auth/login",
+            data={
+                "grant_type": "",
+                "username": "admin",
+                "password": "admin",
+                "scope": "",
+                "client_id": "",
+                "client_secret": "",
+            },
+        )
+
+    async with AsyncClient(app=fastapi_app, base_url=BASE_URL) as ac:
+        response = await ac.post(
+            "/auth/refresh",
+            json={"refresh_token": login_response["refresh_token"]},
+        )
+
+    assert response.status_code == 200
+    response_json: dict = json.loads(response.text)
+    assert "access_token" in response_json.keys()
+    assert "refresh_token" in response_json.keys()
+    assert "token_type" in response_json.keys()
+
+
+@pytest.mark.asyncio
+async def test_expired_token_refresh():
+    await build_db_client()
+
+    # Add any expired token.
+    expired_refresh_token = ""
+
+    async with AsyncClient(app=fastapi_app, base_url=BASE_URL) as ac:
+        response = await ac.post(
+            "/auth/refresh", json={"refresh_token": expired_refresh_token}
+        )
+
+    assert response.status_code == 403
+
+
+@pytest.mark.asyncio
+async def test_invalid_token_refresh():
+    await build_db_client()
+
+    # Add any invalid token.
+    invalid_refresh_token = ""
+
+    async with AsyncClient(app=fastapi_app, base_url=BASE_URL) as ac:
+        response = await ac.post(
+            "/auth/refresh", json={"refresh_token": invalid_refresh_token}
+        )
+
+    assert response.status_code == 403
