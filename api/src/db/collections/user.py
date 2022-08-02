e79633e0f06f79e711ed3460d219d6ade79fdf43
cedrata <81557163+cedrata@users.noreply.github.com>
2022-08-02 22:28:11 +0200

    [UPDATE] users collection document

--- api/src/db/collections/user.py
+++ api/src/db/collections/user.py
@@ -1,12 +1,17 @@
+from datetime import datetime
 from typing import List
 
 from beanie import Document, Indexed
 
 
 class User(Document):
+    email: Indexed(str)
     username: Indexed(str)
     password: str
     roles: List[str]
+    creation: datetime
+    last_update: datetime
+
 
     class Settings:
         name = "users"
