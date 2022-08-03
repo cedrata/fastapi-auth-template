4497373ab7d4d89592c9da64c341504f26ab901f
cedrata <81557163+cedrata@users.noreply.github.com>
2022-08-03 23:50:35 +0200

    [UPDATE] models for user routes. Registration models.

--- api/src/models/commons.py
+++ api/src/models/commons.py
@@ -9,4 +9,5 @@ class BaseMessage(BaseModel):
 
 class HttpExceptionMessage(BaseModel):
     """Message model to specify structure in endpoint response when exception is raised."""
+
     detail: str
--- api/src/models/user.py
+++ api/src/models/user.py
@@ -1,10 +1,54 @@
+import re
 from typing import List
 
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, Field, validator
 
 
-class UserLogin(BaseModel):
+class BaseUser(BaseModel):
+    """Class for representing and validate the atomic part of an user."""
+
+    email: EmailStr = Field(..., description="User email")
+    username: str = Field(..., description="User username")
+
+    @validator("email")
+    def email_validation(cls, email):
+        try:
+            return EmailStr.validate(email)
+        except Exception:
+            raise ValueError(
+                "The email validation was not succesful, the email may be invalid."
+            )
+
+    @validator("username")
+    def username_validation(cls, username):
+        pattern = re.compile("^[a-z0-9._]+$")
+        match = pattern.fullmatch(username)
+        if match is None:
+            raise ValueError(
+                "The username validation was not succesful, the username can only contain alfanumerical chars underscores and dots."
+            )
+        return username
+
+
+class BaseUserRoles(BaseModel):
+    """Class for representing and validate the user roles."""
+
+    roles: List[str] = Field(..., description="Collection of the user roles.")
+
+
+class UserRegistration(BaseUser):
+    """Class for representing the autonomus user registration into the system."""
+
+    password: str = Field(..., description="User password")
+
+
+class UserAdminRegistration(BaseUser, BaseUserRoles):
+    """Class for representin the user registration for admin user, requires at least one role."""
+
+    password: str = Field(..., description="User password")
+
+
+class UserLoginProjection(BaseUser, BaseUserRoles):
     """Class for projection with only username and roles (other attributes if required, no password), from the db users collection."""
 
-    username: str
-    roles: List[str]
+    pass
